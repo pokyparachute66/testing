5656648ec9ab3b88e91d80ac3127465e6903e4a3
Zac <holmebro@gmail.com>
2020-12-08 15:17:14 -0600

    test again

--- main.py
+++ main.py
@@ -4,12 +4,9 @@ for x in range(1, 3):
 for x in range(1, 3):
     print(x+1)
 
-<<<<<<< HEAD
+for x in range(1, 10):
+    print(x)
 
-
-=======
-print(3)
->>>>>>> f2c2b7daf40dfbacfeadc8144f068c9d8d7b3f5c
 for x in range(1,4):
     print(x)
 
